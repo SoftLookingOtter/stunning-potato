9955b6a9f64d091a40600a68a5d6fd48fa58288f
Micke <mikael.dramatic@gmail.com>
2026-05-18 21:56:21 +0200

    WIP: Setup record view

--- Echoes/Echoes/Views/Record/RecordView.swift
+++ Echoes/Echoes/Views/Record/RecordView.swift
@@ -3,5 +3,29 @@
 //  Echoes
 //
 //  Created by Sara Lindén on 2026-05-17.
-//
+//  Updated by Mikael Engvall on 2026-05-18
+
+import SwiftUI
+
+struct RecordView: View {
+    
+    @State private var viewModel = RecordViewModel()
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            
+            Text(viewModel.isRecording ? "Recording..." : "Ready to Record")
+            
+            Button {
+                viewmodel.toggleRecording()
+            } label: {
+                Text(viewModel.isRecording ?  "Stop Recording" : "Start Recording")
+            }
+        }
+    }
+}
+
+#Preview {
+    RecordView()
+}
 
